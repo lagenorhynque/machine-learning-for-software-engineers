5eb7a0371eb069505bbbb229e364c771f934c6da
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2020-03-14 19:17:28 +0900

    Resolve libpython-clj :libary-path dynamically

--- src/ml4se/python.clj
+++ src/ml4se/python.clj
@@ -5,11 +5,32 @@
 
 (def home (System/getenv "HOME"))
 
-(def venv (-> (sh "pipenv" "--venv")
-              :out
-              str/trim-newline
-              (str "/bin/python")))
+(def venv
+  (->> (sh "pipenv" "--venv")
+       :out
+       str/trim-newline))
 
-(py/initialize! :python-executable venv
-                ;; FIXME
-                :library-path (str home "/.pyenv/versions/3.8.1/lib/libpython3.8.dylib"))
+(def venv-python (str venv "/bin/python"))
+
+(def python-version
+  (->> (sh venv-python "--version")
+       :out
+       str/trim-newline))
+
+(def dylib-path
+  (->> (sh "find"
+           (->> (sh "echo" python-version)
+                :out
+                (sh "cut" "-d" " " "-f" "2" :in)
+                :out
+                (str home "/.pyenv/versions/")
+                (str/trim-newline))
+           "-name"
+           "*.dylib")
+       :out
+       (sh "head" "-n" "1" :in)
+       :out
+       str/trim-newline))
+
+(py/initialize! :python-executable venv-python
+                :library-path dylib-path)
